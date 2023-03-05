10b541e71dbb30415ce99529e63b96694c0d2a0b
Renato Gouveia <rpgouveia@gmail.com>
2023-03-05 14:29:24 -0300

    feat: add ResetCSS & StyledContainer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ResetCSS } from './styles/reset'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ResetCSS />
     <App />
   </React.StrictMode>,
 )
