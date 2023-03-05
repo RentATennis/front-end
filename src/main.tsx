73a72e40fe5d227f37b3d1e7078474e40b53fb36
Jorge Sousa <abel.jorge3@outlook.com>
2023-03-05 20:19:29 -0300

    feat: Add empty scafolding to every page and create the theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ResetCSS } from './styles/reset'
+import React from "react"
+import { BrowserRouter } from "react-router-dom"
+import ReactDOM from "react-dom/client"
+import { ThemeProvider } from 'styled-components'
+import App from "./App"
+import { ResetCSS } from "./styles/reset"
+import { mainTheme } from "./styles/theme"
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ResetCSS />
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <ThemeProvider theme={mainTheme}>
+        <ResetCSS />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,13 @@
+import { width } from '@mui/system'
+import React from 'react'
+//import  from '../../assets/'
+
+const LandingPage = () => {
+  return (
+    <>
+      {/* <img src={} alt='Imagem de fundo'/> */}
+    </>
+  )
+}
+
+export default LandingPage
