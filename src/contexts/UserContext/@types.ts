266a9fd4cbb88cb5203ee49df0264a06e7d313fe
Renato Gouveia <rpgouveia@gmail.com>
2023-03-07 16:04:49 -0300

    feat: update loginPage component functionality

---
+++ src/contexts/UserContext/@types.ts
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { iFormLoginValues } from "../../pages/LoginPage/@types";
+
+export interface iUserProvider {
+  children: ReactNode;
+}
+
+export interface iUserContext {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  user: iUser | null;
+  userLogin: (formData: iFormLoginValues) => Promise<void>;
+};
+
+export interface iUser {
+  accessToken: string;
+  user: {
+    email: string;
+    name: string;
+    cpf: string;
+    celular: string;
+    id: number;
+  };
+}
