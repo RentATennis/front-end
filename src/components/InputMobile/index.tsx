6da2c296939101a40572482d475ac94a452d62a7
Renato Gouveia <rpgouveia@gmail.com>
2023-03-11 12:57:30 -0300

    feat: add error treatment to InputMobile

--- src/components/InputMobile/index.tsx
+++ src/components/InputMobile/index.tsx
@@ -1,6 +1,7 @@
 import { TextField } from "@mui/material"
 import TextMask from "react-text-mask"
 import { Controller, Control, FieldValues } from "react-hook-form"
+import { StyledFieldset, StyledErrorMessage } from "./styles"
 
 interface PhoneInputProps {
   control: Control<FieldValues>;
@@ -23,15 +24,19 @@ function InputMobile({ control, name, errors }: PhoneInputProps) {
             guide={false}
             id={name}
             render={(ref: any, props: any) => (
-              <TextField
-                inputRef={ref}
-                {...props}
-                variant="outlined"
-                label="Celular"
-                fullWidth
-                error={errors?.[name] ? true : false}
-                helperText={errors?.[name] ? errors[name].message : null}
-              />
+              <StyledFieldset>
+                <TextField
+                  inputRef={ref}
+                  {...props}
+                  variant="outlined"
+                  label="Celular"
+                  fullWidth
+                  error={errors?.[name] ? true : false}
+                />
+                {errors?.[name] && (
+                  <StyledErrorMessage>{errors[name].message}</StyledErrorMessage>
+                )}
+              </StyledFieldset>
             )}
           />
         )
--- src/components/InputMobile/styles.ts
+++ src/components/InputMobile/styles.ts
@@ -1,16 +1,17 @@
 import styled from "styled-components"
 
-const StyledFieldset = styled.fieldset`
+export const StyledFieldset = styled.fieldset`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 8px;
-
-  p {
-    font-size: var(--font-size-5);
-    font-weight: var(--font-weight-regular);
-    color: var(--color-red);
-  }
 `
 
-export default StyledFieldset
+export const StyledErrorMessage = styled.span`
+  font-family: var(--font-family-1);
+  font-size: var(--font-size-5);
+  font-weight: var(--font-weight-regular);
+  color: var(--color-red);
+  align-self: flex-start;
+  margin-top: 5px;
+`
