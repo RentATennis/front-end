255d49d06dee9be22898328be2df8b11ed88c11d
Renato Gouveia <rpgouveia@gmail.com>
2023-03-13 12:04:23 -0300

    refactor: Login Page, Protected Routes, User Context and Dynamic Modal

--- src/routes/ProtectedRoutes/index.tsx
+++ src/routes/ProtectedRoutes/index.tsx
@@ -2,8 +2,6 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectedRoutes = () => {
   const token = localStorage.getItem('@RentATennis: Token')
-  window.location.pathname
-
   return token ? <Outlet /> : <Navigate to="/login" />
 }
 
